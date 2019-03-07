c47350b616d27db8ba666d6f7685824b2de2d0cd
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-07 22:36:17 +0100

    Utils tests are green

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -10,6 +10,10 @@ public class Utils {
 
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
+  private static final String WINDOWS = "\r\n";
+  private static final String MAC_OS = "\r";
+  private static final String LINUX = "\n";
+
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
    * the next line in the string passed in arguments. 
@@ -20,7 +24,44 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    //store the system used
+    String sys = whichSystem(lines);
+    String[]arrayStr = new String[2];
+
+    if (sys != "") {
+
+      //split the String in two part
+      String[] s = lines.split(sys, 2);
+      arrayStr[0] = s[0] + sys;
+      arrayStr[1] = s[1];
+    }
+    else{
+      arrayStr[0] = "";
+      arrayStr[1] = lines;
+    }
+
+    return arrayStr;
+  }
+
+  /**
+   * Return the name of the use system
+   *
+   * @param s the system used
+   * @return the name of the use system
+   */
+  private static String whichSystem(String s){
+
+    if(s.contains(WINDOWS)){
+      return WINDOWS;
+    }
+    else if(s.contains(MAC_OS)){
+      return MAC_OS;
+    }
+    else if(s.contains(LINUX)){
+      return LINUX;
+    }
+    return "";
   }
 
 }
